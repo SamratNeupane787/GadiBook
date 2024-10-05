a120ca6027cca2aa30d9eaed6950fad7e928dc8e
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-10-05 23:57:06 +0545

    for domain setup

--- app/components/CheckoutForm.jsx
+++ app/components/CheckoutForm.jsx
@@ -33,7 +33,7 @@ function CheckoutForm({ amount }) {
       clientSecret: secretKey,
       elements,
       confirmParams: {
-        return_url: "http://localhost:3000/payment-confirm",
+        return_url: "https://gadibook.samratneupane.com.np/payment-confirm",
       },
     });
   };
